3e09d48ff73009446e467840aa82d73bbbbcf1c8
Derek Jacobs <webfox@bluevulpine.net>
2024-11-13 21:00:09 -0600

    fix: attempt to run envsubst before kubeconform

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -33,7 +33,7 @@ echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
 find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
+    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | envsubst | \
       kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
@@ -44,7 +44,7 @@ echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
 find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
+    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | envsubst | \
       kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
