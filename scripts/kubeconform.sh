0d775e695b86966b4647d2592b19016cbf2de26f
Derek Jacobs <webfox@bluevulpine.net>
2024-11-13 21:19:26 -0600

    fix: kubeconform now needs some default fallbacks with envsubst running

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -33,6 +33,7 @@ echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
 find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
+    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | envsubst
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | envsubst | \
       kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
@@ -44,6 +45,7 @@ echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
 find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
+    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | envsubst
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | envsubst | \
       kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
